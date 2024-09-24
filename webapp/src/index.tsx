309ccd546d21cbb2c542ea098448a59940ebbeeb
Boris Georgiev <bobi591@gmail.com>
2024-09-24 22:25:06 +0300

    Revert "Update with react-snap"
    This reverts commit 298b01d85bc44a88d3e0d19aaa45b218ff06a113.

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
 import { lazy, Suspense } from 'react'
-import { hydrate, render } from 'react-dom';
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
@@ -21,7 +21,6 @@ import InformationPageGalleryBottom from './components/pages/informationGalleryB
 import './index.css'
 import { AppStore } from './ReduxStore'
 import reportWebVitals from './reportWebVitals'
-import React from 'react'
 
 const App = lazy(() => import('./App'))
 const HomePage = lazy(() => import('./components/pages/home/HomePage'))
@@ -29,7 +28,7 @@ const NotFoundPage = lazy(
     () => import('./components/pages/notfound/NotFoundPage')
 )
 
-const root = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 TimeAgo.addDefaultLocale(en)
 
@@ -112,47 +111,23 @@ const router = createBrowserRouter([
     },
 ])
 
-if(root?.hasChildNodes()) {
-    hydrate(
-        <React.StrictMode>
-            <Suspense>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="anonymous"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
-                    rel="stylesheet"
-                />
-                <Provider store={AppStore}>
-                    <RouterProvider router={router} />
-                </Provider>
-            </Suspense>
-        </React.StrictMode>, root)
-}
-else {
-    render(
-        <React.StrictMode>
-            <Suspense>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="anonymous"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
-                    rel="stylesheet"
-                />
-                <Provider store={AppStore}>
-                    <RouterProvider router={router} />
-                </Provider>
-            </Suspense>
-        </React.StrictMode>, root
-    )
-}
+root.render(
+    <Suspense>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+        />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+        />
+        <Provider store={AppStore}>
+            <RouterProvider router={router} />
+        </Provider>
+    </Suspense>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
