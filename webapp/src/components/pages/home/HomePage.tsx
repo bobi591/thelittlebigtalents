94d9d3dd433c302e2e80c679c4f3fd612f9ee5d2
Boris Georgiev <bgeorgiev@pros.com>
2024-11-18 15:19:37 +0200

    Close Signup when clicking send

--- webapp/src/components/pages/home/HomePage.tsx
+++ webapp/src/components/pages/home/HomePage.tsx
@@ -158,7 +158,7 @@ export default class App extends React.Component {
                     >
                         Отмяна
                     </Button>
-                    <Button type="submit" variant="primary">
+                    <Button type="submit" variant="primary" onClick={() => this.showBookLessonModal(false)}>
                         Изпрати
                     </Button>
                 </Modal.Footer>
