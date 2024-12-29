75b51379b9656bbada39f41a4b8ee61c6aa59b17
Boris Georgiev <bobi591@gmail.com>
2024-12-29 17:30:27 +0200

    Lint fix

--- webapp/src/components/pages/home/HomePage.tsx
+++ webapp/src/components/pages/home/HomePage.tsx
@@ -190,71 +190,23 @@ export default class App extends React.Component {
                 >
                     {this.carouselOverlay}
                     <Carousel controls={false} interval={5000} wrap={true}>
-                        <Carousel.Item>
-                            <img
-                                className="carousel-picture"
-                                src={AzureBlobStorage.getBlobUrl(
-                                    'home-carousel-1.jpg'
-                                )}
-                                style={{
-                                    objectFit: 'cover',
-                                    height: '80vh',
-                                    width: '100vw',
-                                }}
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="carousel-picture"
-                                src={AzureBlobStorage.getBlobUrl(
-                                    'home-carousel-2.jpg'
-                                )}
-                                style={{
-                                    objectFit: 'cover',
-                                    height: '80vh',
-                                    width: '100vw',
-                                }}
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="carousel-picture"
-                                src={AzureBlobStorage.getBlobUrl(
-                                    'home-carousel-3.jpg'
-                                )}
-                                style={{
-                                    objectFit: 'cover',
-                                    height: '80vh',
-                                    width: '100vw',
-                                }}
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="carousel-picture"
-                                src={AzureBlobStorage.getBlobUrl(
-                                    'home-carousel-4.jpg'
-                                )}
-                                style={{
-                                    objectFit: 'cover',
-                                    height: '80vh',
-                                    width: '100vw',
-                                }}
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="carousel-picture"
-                                src={AzureBlobStorage.getBlobUrl(
-                                    'home-carousel-5.jpg'
-                                )}
-                                style={{
-                                    objectFit: 'cover',
-                                    height: '80vh',
-                                    width: '100vw',
-                                }}
-                            />
-                        </Carousel.Item>
+                        {Array.from({ length: 5 }).map((_, index) => {
+                            return (
+                                <Carousel.Item>
+                                    <img
+                                        className="carousel-picture"
+                                        src={AzureBlobStorage.getBlobUrl(
+                                            `home-carousel-${index + 1}.jpg`
+                                        )}
+                                        style={{
+                                            objectFit: 'cover',
+                                            height: '80vh',
+                                            width: '100vw',
+                                        }}
+                                    />
+                                </Carousel.Item>
+                            )
+                        })}
                     </Carousel>
                 </div>
                 <div className="d-flex-inlne justify-content-center homepage-information">
